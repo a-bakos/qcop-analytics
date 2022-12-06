3fbd78e60129701c0db37c62430a7d5e075792d9
a-bakos <bakosattila@fastem.com>
2022-12-06 19:55:54 +0000

    Define initial data structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod search_query;
+
+use search_query::*;
+
 fn main() {
-    println!("Hello, world!");
+    let query_test: SearchQuery = SearchQuery::new((
+        String::from("2018-08-21 17:19:46"),
+        String::from("sharing your work"),
+        String::from("https%3A%2F%2Fauthorservices.taylorandfrancis.com%2F"),
+        32,
+        String::from("https://authorservices.taylorandfrancis.com/sharing-your-work"),
+    ));
+
+    println!("{:#?}", query_test);
 }
