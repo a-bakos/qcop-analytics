cbbc774604912228881533f3398de7f6fc7ed8ec
Attila Bakos <bakosattila@fastem.com>
2023-09-13 08:44:03 +0100

    Update intelligence.rs

--- src/intelligence.rs
+++ src/intelligence.rs
@@ -2,8 +2,11 @@
 // - X number of most searched for keywords between date1 and date2
 // - X number of pages where search attempts happen
 // - X number of most active target page
+// - most popular source for a particular keyword
+// - most popular target for a particular keyword
 // - ratio of click-through per all same keyword search
 // - X number of most searched keywords with no matching hits
-
-// later stage
+// - trending keywords (in a range, eg last month ?)
+// - trending targets (in a range, eg last month ?)
 // - get insights for provided keyword
+// - ideas for potential FAQ page content
